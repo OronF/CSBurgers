a41c5ecf4f5c6e71287f207374ada1aa86604911
OronF <oron.fink@gmail.com>
2023-08-25 00:38:11 +0300

    adding the history for the chat

--- public/javascripts/contactUs.js
+++ public/javascripts/contactUs.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     let index = 1;
 
     const createSelection = (branch) => {
-        framework.append(`<option value="${index}" data-manager-id="${branch._id}">${branch.name}</option>`);
+        framework.append(`<option value="${index}" data-manager-id="${branch.manager}">${branch.name}</option>`);
         index++;
     }
 
