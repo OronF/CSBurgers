91f9c8209774dae86eb632e80e51f0753bde298a
AmitOvits <toniovits@gmail.com>
2023-08-28 02:31:38 +0300

    adding errors to forgotpassword

--- public/javascripts/forgotPassword.js
+++ public/javascripts/forgotPassword.js
@@ -1,3 +1,11 @@
+function checkIfEmpty(inp, Error)
+{
+    if(inp === "")
+    {
+        Error.html("שדה ריק אינו חוקי");
+        return true;
+    }
+}
 $(document).ready(function() {
 
     const approve = $('#approveBtn');
@@ -12,6 +20,16 @@ $(document).ready(function() {
         const lnameVal = lnameTxt.val();
         const passwordVal = passwordTxt.val();
         const passwordApproveVal = passwordApproveTxt.val();
+
+        const fnameError = $('#fnameError');
+        const lnameError = $('#lnameError');
+        const passwordError = $('#passwordError');
+        const approvePasswordError = $('#approvePasswordError');
+        checkIfEmpty(fnameVal, fnameError);
+        checkIfEmpty(lnameVal, lnameError);
+        checkIfEmpty(passwordVal, passwordError);
+        checkIfEmpty(passwordApproveVal, approvePasswordError);
+
 
         var user;
 
@@ -37,8 +55,32 @@ $(document).ready(function() {
 
             return;
         }
+        if (passwordApproveVal !== passwordVal) {
+            approvePasswordError.html("סיסמאות לא שוות")
+            flagChangePassword = 0;
+        }
+        if (fnameVal.length > 10) {
+            fnameError.html( "הזנת שם פרטי ארוך מדי");
+            flagChangePassword = 0;
+        } 
+        
+        if (lnameVal.length > 10){
+            lnameError.html("הזנת שם משפחה ארוך מדי");
+            flagChangePassword = 0;
+        } 
+        
+        //if (!checkIfPhoneNumberIsValid(phoneNumberVal)){
+        //    phoneNumberError.html("הזנת מספר טלפון לא חוקי");
+        //    flagChangePassword = 0;
+        //}
 
-        if (passwordVal.length > 8) {
+
+        if(passwordApproveVal !== passwordVal){
+            approvePasswordError.html("שדה אישור הסיסמה אינו זהה לסיסמה")
+            passwordError.html("שדה אישור הסיסמה אינו זהה לסיסמה")
+            flagChangePassword = 0;
+        }
+        if (passwordVal.length >= 8) {
             let counter = 0;
 
             for (let i = 0; i < passwordVal.length; i++) {
@@ -51,32 +93,32 @@ $(document).ready(function() {
                 return;
             }
         } else {
-            return;
+            passwordError.html("על הסיסמה להכיל לפחות 8 תווים");
+            flagChangePassword = 0;
         }
 
-        if (passwordApproveVal !== passwordVal) {
-            return;
-        }
 
-        $.ajax({
-            url:`/api/user/${user[0]._id}`,
-            method: "PUT",
-            dataType: "json",
-            contentType: 'application/json',
-            data: JSON.stringify({
-                fname: user[0].fname,
-                lname: user[0].lname,
-                orders: user[0].orders,
-                phoneNumber: user[0].phoneNumber,
-                password: passwordVal,
-                is_Manager: user[0].is_Manager
-            }),
-            success: function(response) {
-                console.log("Data saved successfully:", response);
-            },
-            error: function(error) {
-                console.error("Error saving data:", error);
-            }
-        });
+        if(flagChangePassword != 0){
+            $.ajax({
+                url:`/api/user/${user[0]._id}`,
+                method: "PUT",
+                dataType: "json",
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    fname: user[0].fname,
+                    lname: user[0].lname,
+                    orders: user[0].orders,
+                    phoneNumber: user[0].phoneNumber,
+                    password: passwordVal,
+                    is_Manager: user[0].is_Manager
+                }),
+                success: function(response) {
+                    console.log("Data saved successfully:", response);
+                },
+                error: function(error) {
+                    console.error("Error saving data:", error);
+                }
+            });
+        }
     });
 });
