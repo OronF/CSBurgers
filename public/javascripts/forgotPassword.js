654336a9ad35fb2c96067d57b9b41a7c3910766d
RoniYamin <yaminronis@gmail.com>
2023-08-29 03:02:39 +0300

    fixing problems with the phone number

--- public/javascripts/forgotPassword.js
+++ public/javascripts/forgotPassword.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
         const lnameTxt = $('#lname');
         const passwordTxt = $('#password');
         const passwordApproveTxt = $('#passwordApprov');
-        const PhoneNumberTxt = $('PhoneNumber');
+        const PhoneNumberTxt = $('phone-number');
 
         const fnameVal = fnameTxt.val();
         const lnameVal = lnameTxt.val();
@@ -86,6 +86,10 @@ $(document).ready(function() {
             flagChangePassword = 0;
         } 
 
+        if (!checkIfPhoneNumberIsValid(PhoneNumberVal)){
+            phoneNumberError.html("הזנת מספר טלפון לא חוקי");
+            flagChangePassword = 0;
+        }
 
         if(passwordApproveVal !== passwordVal){
             approvePasswordError.html("שדה אישור הסיסמה אינו זהה לסיסמה")
@@ -134,3 +138,67 @@ $(document).ready(function() {
         }
     });
 });
+
+/************************************** phone number formator **************************************/
+const phoneNumberField = document.getElementById("phone-number");
+
+
+phoneNumberField.addEventListener("select", function(event) {
+    event.preventDefault(); // Prevent text selection
+ });
+ 
+
+    // Disable arrow keys and text selection
+    phoneNumberField.addEventListener('keydown', function(event) {
+      if (event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        event.preventDefault();
+      }
+    });
+
+    phoneNumberField.addEventListener("select", function(event) {
+        event.preventDefault(); // Prevent text selection
+      });
+
+phoneNumberField.addEventListener("click", function() {
+    if (phoneNumberField.value.length === 0) {
+        phoneNumberField.value = "05";
+    }
+});
+
+phoneNumberField.addEventListener("keydown", function(event) {
+    if ((event.key === "Backspace" || event.key === "Delete") && phoneNumberField.value.length === 2) {
+        event.preventDefault();
+    }
+});
+
+document.addEventListener("click", function(event) {
+    if (event.target !== phoneNumberField && phoneNumberField.value.length === 2) {
+        phoneNumberField.value = "";
+    }
+});
+
+
+function phoneNumberFormatter()
+{
+    const phoneNumberField = document.getElementById("phone-number");
+    const formattedInputVal = formatPhoneNumber(phoneNumberField.value);
+    phoneNumberField.value = formattedInputVal;
+    
+}
+function formatPhoneNumber(value)
+{
+    if(!value) return value;
+    
+    const phoneNumber = value.replace(/[^\d]/g, '');
+    const phoneNumberLength = phoneNumber.length;
+    
+    if(phoneNumberLength < 4) return phoneNumber;
+    if(phoneNumberLength < 7){
+        return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(
+        3,
+        6,
+        )}-${phoneNumber.slice(6,9)
+        }`;
+}
